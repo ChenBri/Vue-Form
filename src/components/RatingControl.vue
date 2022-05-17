227cc7a90bac7667a123a5d8ffab99eda8d83f8b
Chen Brilling <chenbrilling@gmail.com>
2022-05-18 01:51:40 +0300

    adding the form

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,40 @@
+<template>
+    <ul>
+        <li :class="{active: modelValue === 'poor'}">
+            <button type="button" @click="activate('poor')">Poor</button>
+        </li>
+        <li :class="{active: modelValue === 'average'}">
+           <button type="button" @click="activate('average')">Average</button>
+        </li>
+        <li :class="{active: modelValue === 'great'}">
+            <button type="button" @click="activate('great')">Great</button>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    props:[
+        'modelValue'
+    ],
+    emits: [
+        'update:modelValue'
+    ],
+    methods:{
+        activate(option){
+            this.$emit('update:modelValue', option)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.active{
+    border-color: purple;
+}
+
+.active button {
+    color: purple;
+}
+
+
+</style>
